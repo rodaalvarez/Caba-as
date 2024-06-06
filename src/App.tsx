e57f9dedc2505d07ab27dec22e9f8379de3df012
ZEXTRAlvarez <rodrigo.alvarez@andromedalatam.com>
2024-06-06 14:23:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './pages/WelcomePage';
+import Welcome from './features/Welcome/Welcome';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>        
           <Route path="/" element={<WelcomePage />} />
+          <Welcome/>
         </Routes>
       </Router>
     
